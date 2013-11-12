58e9776f6030bbe300db394574318c20afab5d40
hendrikwout <hendrikwout@gmail.com>
2013-11-12 10:33:48 +0100

    replace pcl to pcd

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pynacolada',
-    version='0.2.3',
+    version='0.2.4',
     author='H. Wouters',
     author_email='hendrikwout@gmail.com',
     packages=['pynacolada'],
