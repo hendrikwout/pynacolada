5b363bd6b517ef159898a3b9ea0b822c8298acab
hendrikwout <hendrikwout@gmail.com>
2021-02-01 14:48:11 +0100

    test

--- old/examples/examplecsv2netcdf.py
+++ old/examples/examplecsv2netcdf.py
@@ -3,7 +3,7 @@ import numpy as np
 import datetime as dt
 from Scientific.IO import NetCDF
 
-csvfile = open('data/example.csv','r')
+csvfile = open('data/example.csv', 'r')
 ncfile = NetCDF.NetCDFFile('data/example.nc','w')
 
 pcd.csv2netcdf(csvfile,ncfile,sep=',',formatlist=[[dt.datetime,'%m/%d/%Y %H:%M']]+[[np.double,np.nan]]*15,refdat=dt.datetime(2009,1,1,0,0), tunits='hours')
