e06ee12985655faa47e968e8d3ca67daf6eaccdd
hendrikwout <hendrikwout@gmail.com>
2021-10-08 12:56:11 +0200

    reimplement propagate reset_lock reset_achive and force_recalculate arguments

--- pynacolada/__init__.py
+++ pynacolada/__init__.py
@@ -3,6 +3,7 @@ from .apply_func_new import apply_func,apply_func_per_group
 import pynacolada.io
 import pynacolada.vectorized_functions
 from  .archive import *
+from  .broker import *
 
 from .apply_func import apply_func as apply_func_old
 from .apply_func import apply_func_per_group as apply_func_per_group_old
---
+++ pynacolada/broker.py
@@ -0,0 +1,190 @@
+import logging
+from . import archive,collection
+
+
+class broker (object):
+    def __init__(self,**kwargs):
+        for key,value in kwargs.items():
+            self.__dict__[key] = value
+        self.global_settings()#default settings
+
+    def propagate_arguments_to_parents(self):
+        logging.info('BEGIN --- propagate arguments to parent processes --- ')
+        for ibroker_requires, broker_requires in enumerate(self.requires):
+            for key in broker_requires.keys():
+                if (broker_requires[key] is None) or (type(broker_requires[key]) is int):
+                    iselect= (0 if (broker_requires[key] is None) else broker_requires[key])
+                    if key in self.provides[iselect].keys():
+                        self.requires[ibroker_requires][key] = self.provides[iselect][key]
+                    # elif key in vars(args).keys():
+                    #     self.requires[ibroker_requires][key] = vars(args)[key]
+                    else:
+                        del self.requires[ibroker_requires][key]
+                elif type(broker_requires[key]) is type(lambda x:x):
+                    pass_parameters = []
+                    for ibroker_provides, broker_provides in enumerate(self.provides):
+                        pass_parameters.append(broker_provides[key])
+                    self.requires[ibroker_requires][key] = self.requires[ibroker_requires][key](pass_parameters)
+        logging.info('END --- propagate arguments to parent processes --- ')
+
+    def global_settings(self,**kwargs):
+        self.global_settings_defaults = {
+            'conda_environment_export': 'blabla',
+            'dummy': 'False',
+            'reset_archive': 0,
+            'reset_lock':0,
+            'force_recalculate':0,
+        }
+
+        for setting in self.global_settings_defaults.keys():
+            if setting in kwargs.keys():
+                self.__dict__[setting] = kwargs[setting]
+
+    def retrieve_input(self,args):
+        logging.info('--- BEGIN Collecting or generating coarse input data -- ')
+        for ibroker_requires, broker_requires in enumerate(self.requires):
+
+            request_parent = self.requires[ibroker_requires].copy()
+            for key, item in list(request_parent.items()):
+                if type(item) is type(lambda x: x):
+                    del request_parent[key]
+                if key in ['archive', 'process','root']:
+                    del request_parent[key]
+
+            if 'process' in broker_requires:
+                #print('broker_requires', broker_requires)
+                #print('request_parent', request_parent)
+                # parent_execute = ['conda','init','bash','&&','conda','activate','KLIMPALA','&&', 'python',broker_requires['process']]
+                # parent_execute = ['source','/projects/C3S_EUBiodiversity/bio_load.sh',';','python',broker_requires['process']]
+                # parent_execute = ['python',broker_requires['process']]
+                parent_execute = [
+                    './bin/launch_python_conda.sh',
+                    self.conda_environment_export,
+                    broker_requires['process']
+                ]
+                # parent_execute = ['source', '/projects/C3S_EUBiodiversity/bio_load.sh','&&', 'python',broker_requires['process']]
+                parent_arguments = []
+
+                for key, value in request_parent.items():
+                    # if key != 'spaces':
+                    parent_arguments.append('--' + key)
+                    # if type(value) not in [int,bool]:
+                    #     parent_execute.append('"'+str(value)+'"')
+                    # else:
+                    parent_arguments.append(str(value))
+
+                logging.info('propagate environment settings')
+                arguments_propagate_to_parent = ['conda_environment_export', 'dummy']
+                for argument in arguments_propagate_to_parent:
+                    parent_arguments.append('--' + key)
+                    parant_arguments.append(self.__dict__[key] if (self.__dict__[key] != '') else '""')
+
+                arguments_propagate_reduce_to_parent = [
+                    'reset_archive',
+                    'reset_lock',
+                    'force_recalculate'
+                ]
+                for key in arguments_propagate_reduce_to_parent:
+                    parent_execute.append('--' + key)
+                    parent_execute.append(max(self.__dict__[key]-1, 0))
+
+                # p = Popen(parent_arguments , stdout=PIPE, stderr=PIPE)
+
+                for iarg, arg in enumerate(parent_execute):
+                    if arg == "":
+                        parent_execute[iarg] = '""'
+                # logging.info('Executing parent process:'+ (' '.join(parent_execute))+'"'+ '" "'.join(parent_arguments))
+                loggin.info('parent_execute: ', parent_execute)
+                logging.info('parent_arguments: ', parent_arguments)
+                tempbasename = tempfile.mktemp(prefix=broker_requires['process'].split('/')[-1] + '_')
+                self.requires[ibroker_requires]['stderr'] = open(tempbasename + '_e.log', 'w')
+                self.requires[ibroker_requires]['stdout'] = open(tempbasename + '_o.log', 'w')
+
+                logging.info('- stdout: ' + self.requires[ibroker_requires]['stdout'].name)
+                logging.info('- stderr: ' + self.requires[ibroker_requires]['stderr'].name)
+                print(parent_execute + parent_arguments)
+                self.requires[ibroker_requires]['executing_subprocess'] = \
+                    Popen(
+                        parent_execute + parent_arguments,
+                        # ' '.join(parent_execute), #' "' + ('" "'.join(parent_arguments))+'" ',
+                        # (' '.join(parent_execute) + '"' + '" "'.join(parent_arguments)),
+                        stdout=self.requires[ibroker_requires]['stdout'],
+                        stderr=self.requires[ibroker_requires]['stderr'],
+                        )
+        for ibroker_requires,broker_requires in enumerate(self.requires):
+            if 'executing_subprocess' in broker_requires:
+                broker_requires['executing_subprocess'].wait()
+                self.requires[ibroker_requires]['stderr'].close()
+                self.requires[ibroker_requires]['stdout'].close()
+                return_from_subprocess = open(self.requires[ibroker_requires]['stdout'].name,'r').readlines()[-1][:-1]
+                print('retrieving from parent process '+self.requires[ibroker_requires]['stdout'].name+': '+return_from_subprocess)
+
+                self.requires[ibroker_requires].update(literal_eval(return_from_subprocess))
+
+        for ibroker_provides, broker_provides in enumerate(self.provides):
+            if ('archive' in broker_provides) and (type(broker_provides['archive']) is type(lambda x:x)):
+                values_input = [ item['archive'] for item in self.requires]
+                self.provides[ibroker_provides]['archive'] = broker_provides['archive'](values_input)
+
+        if args.reset_archive > 0 :
+            logging.info('Resetting archive of current broker')
+            for broker_current in self.provides:
+                if 'archive' in broker_current.keys():
+                    if type(broker_current['archive']) is str:
+                        archive_out = archive(args.root + '/' + broker_current['archive'])
+                        archive_out.remove(records=True)
+                        archive_out.close()
+                    elif type(broker_current['archive']) is archive:
+                        broker_current['archive'].remove(records=True)
+                        broker_current['archive'].close()
+
+        self.parent_collection = collection(
+            [pcd.archive(args.root_requires + '/' + broker_requires['archive']) for broker_requires in
+             self.requires]
+        )
+        logging.info('--- END Collecting or generating coarse input data --- ')
+
+    def apply_func(self,*args,**kwargs):
+        #for ibroker_provides, broker_provides in enumerate(self.provides):
+        archive_out_filename =self.provides[0]['root'] + '/' + self.provides[0]['archive']
+        lockfile = archive_out_filename + '_lock'
+        if self.reset_lock > 0:
+            os.system('rm ' + lockfile)
+        while os.path.isfile(lockfile):
+            logging.info('archive locked with "' + lockfile + '" . Waiting for another process to release it.')
+            sleep(10)
+
+
+        requests_parents = [
+            broker_requires.copy() for broker_requires in broker.requires
+        ]
+        requests_parents = [broker_requires.copy() for broker_requires in broker['requires']]
+
+        for irequest_parent,request_parent in enumerate(requests_parents):
+            for key,value in list(request_parent.items()):
+                if \
+                        (key in ['archive','process','executing_subprocess','stderr','stdout']) or \
+                            (key not in broker_parent_collection.get_lib_dataarrays().columns and \
+                        key not in broker_parent_collection.get_lib_dataarrays().index.names) or \
+                        type(value) is type(lambda x: x):
+                    del requests_parents[irequest_parent][key]
+
+        apply_groups_out = dict(self.provides[0])
+        del apply_groups_out['archive']
+        self.parent_collection.apply_func(
+            self.operator,
+            apply_groups_in = request_parents,
+            apply_groups_out=[apply_groups_out],
+            archive_out = archive_out_filename,
+            force_recalculate = (self.force_recalculate> 0),
+            *args,
+            **kwargs)
+        os.system('rm '+lockfile)
+
+        logging.info('Dumping return_request as last line in stdout being used for processes depending on it')
+        request_return = dict(apply_groups_out)
+        request_return['archive'] = broker.provides[0]['archive']
+        for key, value in list(request_return.items()):
+            if type(value) is type(lambda x:x):
+                del request_return[key]
+        return str(request_return).replace(' ','')
