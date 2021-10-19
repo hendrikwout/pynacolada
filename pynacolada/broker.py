9b65003bca12cd4d433f2d2a65482b4ca5684282
hendrikwout <hendrikwout@gmail.com>
2021-10-19 20:32:34 +0200

    eliminate non-index data selection

--- pynacolada/broker.py
+++ pynacolada/broker.py
@@ -221,13 +221,13 @@ class broker (object):
                             apply_groups_out[key].append(self.provides[key][iapply_groups_out])
                     else:
                         apply_groups_out[key] = self.provides[key]
-        if debug == True:
-            import pdb; pdb.set_trace()
 
         for irequest_parent,request_parent in list(enumerate(requests_parents)):
             for key in list(requests_parents[irequest_parent]):
                 if key not in self.parent_collection.get_lib_dataarrays().index.names:# ['variable','source','time','space']:
                     del requests_parents[irequest_parent][key]
+        if debug == True:
+            import pdb; pdb.set_trace()
 
         self.parent_collection.apply_func(
             self.operator,
