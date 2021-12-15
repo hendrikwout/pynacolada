2cbe6dfc1fe77aa52f973c6a938cb6dbcc0ab1a5
hendrikwout <hendrikwout@gmail.com>
2021-12-15 23:55:58 +0100

    inherit argument keys from parent

--- pynacolada/apply_func_new.py
+++ pynacolada/apply_func_new.py
@@ -76,6 +76,7 @@ def apply_func(func,xarrays,dims_apply, method_dims_no_apply='outer',filenames_o
                 dimslib[dim] = dimslib_temp
             else:
                 import pdb; pdb.set_trace()
+
                 if not ((len(dimslib_temp) == 1) or (dim in dims_apply_def) or (len(dimslib_temp) == len(dimslib[dim])) ):
                     raise ValueError('dimension '+str(dimslib_temp)+' of xarray '+str(xarray) +'is not consistent with dimension '+dimslib[dim]+' occurring in other input arrays. Please check. ')
         logger.debug('dimslib result: '+str(dimslib))
