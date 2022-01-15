6bf4804c3f7da8dd60d58cfdcb37ea62c4ac0ce7
hendrikwout <hendrikwout@gmail.com>
2022-01-16 00:27:17 +0100

    work on calc_extreme_precipitation.py

--- pynacolada/vectorized_functions.py
+++ pynacolada/vectorized_functions.py
@@ -270,6 +270,7 @@ def interp1d(x_fix, y_fix, x_var):
     # x_fix = x_fix.reshape([1]*(len(x_var.shape) - len(x_fix.shape))+list(x_fix.shape))
     # y_fix = y_fix.reshape([1]*(len(x_var.shape) - len(x_fix.shape))+list(y_fix.shape))
 
+    import pdb; pdb.set_trace()
     #workaround flawed implementation apply_func
     if x_fix.shape[-2] != 1:
         x_fix = x_fix.reshape(list(x_fix.shape[:-1])+[1]+[x_fix.shape[-1]])
@@ -278,6 +279,7 @@ def interp1d(x_fix, y_fix, x_var):
     if x_var.shape[-1] != 1:
         x_var = x_var.reshape(list(x_var.shape)+[1])
 
+    import pdb; pdb.set_trace()
     # if 1 not in x_fix.shape:
     #     start_shape_weights = 0
     # else:
