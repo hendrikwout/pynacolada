58f6d20eba19589d939d2d48def5e731d7a68afc
Hendrik Wouters <hendrikwout@gmail.com>
2022-02-17 12:46:51 +0100

    optimize downscale orography

--- pynacolada/vectorized_functions.py
+++ pynacolada/vectorized_functions.py
@@ -207,7 +207,7 @@ def extend_crop_interpolate(
             )
             if workaround_2_dim:
                 x_interpolated = x_interpolated[0]
-        if debug:
+        if debug == True:
             import pdb; pdb.set_trace()
     # x_interpolated = pcd.vectorized_functions.interpolate_delaunay_linear(
     #     x_extended,
