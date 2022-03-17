27ec006223d0ba82122c287b6ce6c2da5bb4fc59
Hendrik Wouters <hendrikwout@gmail.com>
2022-03-17 12:21:47 +0100

    add downscaling for rainy seasons

--- pynacolada/archive.py
+++ pynacolada/archive.py
@@ -112,7 +112,8 @@ def apply_func_wrapper(
 
         apply_this_group_in_df = apply_groups_in_df.copy()
 
-        for idx_group_in, row in apply_this_group_in_df.iterrows():
+        #for idx_group_in, row in apply_this_group_in_df.iterrows():
+        for idx_group_in, row in enumerate(apply_this_group_in_df.to_dict('records')):
             for column in apply_this_group_in_df.columns:
                 if row[column] is None:
                     # import pdb;pdb.set_trace()
