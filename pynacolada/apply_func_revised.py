ea4cfcc9d9094ac2c4de287326e3c15b2f430039
Hendrik Wouters <hendrikwout@gmail.com>
2022-05-09 22:06:59 +0200

    apply func revised

--- pynacolada/apply_func_revised.py
+++ pynacolada/apply_func_revised.py
@@ -7,6 +7,7 @@ import xarray as xr
 #from rasterio.windows import Window
 import pandas as pd
 import numpy as np
+import tqdm
 import logging
 #ProgressBar().register()
 class RIOFile(object):
@@ -69,8 +70,8 @@ dims_apply_names = ['latitude','longitude']
 
 
 dims_output = {
-        'latitude':{ 'coordinates':ds.latitude,'chunksize':500},
-        'longitude':{ 'coordinates':ds.longitude,'chunksize':500},
+        'latitude':{ 'coordinates':ds.latitude,'chunksize':5000},
+        'longitude':{ 'coordinates':ds.longitude,'chunksize':5000},
         }
 
 logging.debug('determining inner number of chunks')
@@ -87,10 +88,10 @@ number_of_chunks_apply = int(number_of_chunks_apply)
 def identical_xarrays(xarray1,xarray2):
    return (xarray1.shape == xarray2.shape) and (not (np.any(xarray1 != xarray2))) 
 
-logging.debug('collecting the dimensions occuring in the xarrays over which the function is not applied')
+logging.info('collecting the dimensions occuring in the xarrays over which the function is not applied')
 dims_no_apply  = {}
 
-logging.debug('Adding extra chunking as a separate dimension')
+logging.info('Adding extra chunking as a separate dimension')
 if number_of_chunks_apply > 1:
     dims_no_apply['__chunk__'] = xr.DataArray(range(number_of_chunks_apply))
 
@@ -107,7 +108,7 @@ dims_all = list(dims_no_apply.keys()) + dims_apply_names
 dims_no_apply_lengths = { name:dim.shape[0] for (name,dim) in dims_no_apply.items()}
 # dims_apply_shapes = { name:dim.shape for (name,dim) in dims_apply_names.items()}
 #dims_all_shapes = {**dims_no_apply_shapes,**dims_apply_shapes}
-logging.debug('determining chunked shapes for each xarray')
+logging.info('determining chunked shapes for each xarray')
 
 xarrays_shapes_in_chunks = [list() for i in range(len(xarrays))]
 xarrays_shapes = [list() for i in range(len(xarrays))]
@@ -117,7 +118,7 @@ xarrays_shapes = [list() for i in range(len(xarrays))]
 xarrays_chunks_apply = [False]*len(xarrays)
 for ixarray,xarray in enumerate(xarrays):
     for idim,dimname in reversed(list(enumerate(dims_apply_names))):
-        print(dimname,ixarray,xarrays_shapes_in_chunks)
+        #print(dimname,ixarray,xarrays_shapes_in_chunks)
         if dimname in xarray.dims:
             if ( 
                     (dimname in dims_output) and 
@@ -156,7 +157,9 @@ if number_of_chunks_apply > 1:
             xarrays_shapes_in_chunks[ixarray].insert(0,None)
         xarrays_shapes[ixarray].insert(0,None)
 
-logging.debug('chunked shapes for '+str(dims_all)+': '+str(xarrays_shapes_in_chunks))
+logging.info('xarrays shapes for '+str(dims_no_apply.keys()) +' + '+str(dims_apply_names)+' : ')
+logging.info('  -> original xarrays: '+str(xarrays_shapes))
+logging.info('  ->  chunked xarrays: '+str(xarrays_shapes_in_chunks))
 
 maximum_memory_size_input = 10**8
 logging.info('determining input chunk format that fits our maximum memory size input of '+str(maximum_memory_size_input))
@@ -169,6 +172,9 @@ for idim,dimname in iteration_over_apply_dims:
         if xarrays_shapes_in_chunks[ixarray][idim] != None:
             chunks_memory_sizes[ixarray] *= xarrays_shapes_in_chunks[ixarray][idim]
             chunks_memory_sizes_dim[ixarray].insert(0,xarrays_shapes_in_chunks[ixarray][idim])
+
+logging.info('xarray chunk memory sizes: '+ str(chunks_memory_sizes))
+logging.info('xarray chunk memory size per dimension (last one is character byte size): '+ str(chunks_memory_sizes_dim))
 
 iteration_over_noapply_dims = list(reversed(list(enumerate(dims_all))[:len(dims_no_apply)]))
 current_memory_size  = sum(chunks_memory_sizes)
@@ -186,26 +192,31 @@ for idim,dimname in iteration_over_noapply_dims:
             else:
                 xarrays_sized_cumulative_mul += chunks_memory_sizes[ixarray]
 
-        if xarrays_sized_cumulative_mul  != 0:
-            chunk_sizes_no_apply[dimname] = np.floor(np.min([(maximum_memory_size_input - xarrays_sized_cumulative_base)/xarrays_sized_cumulative_mul, dims_no_apply_lengths[dimname]]))
+        if dimname == '__chunk__':
+            logging.debug('We do not allow grouping of apply-dimension chunking.')
+            chunk_sizes_no_apply[dimname] = 1.0
         else:
-            chunk_sizes_no_apply[dimname] = 0
-
+            if xarrays_sized_cumulative_mul  != 0:
+                chunk_sizes_no_apply[dimname] = np.floor(np.min([(maximum_memory_size_input - xarrays_sized_cumulative_base)/xarrays_sized_cumulative_mul, dims_no_apply_lengths[dimname]]))
+            else:
+                chunk_sizes_no_apply[dimname] = 0
 
         if not chunk_sizes_no_apply[dimname].is_integer():
             raise ValueError('whole number expected for dimension selection size')
 
+        current_memory_size = int((xarrays_sized_cumulative_base + xarrays_sized_cumulative_mul * chunk_sizes_no_apply[dimname]))
+        #print(current_memory_size > maximum_memory_size_input)
+        #import pdb; pdb.set_trace()
+        # if current_memory_size > maximum_memory_size_input:
+        #     raise ValueError('something wrong with the chunk size calculation for limiting memory')
         for ixarray,xarray in enumerate(xarrays):
             if xarrays_shapes_in_chunks[ixarray][idim] != None:
                 chunks_memory_sizes[ixarray] *= chunk_sizes_no_apply[dimname]
                 chunks_memory_sizes_dim[ixarray].insert(0,chunk_sizes_no_apply[dimname])
             else:
                 chunks_memory_sizes_dim[ixarray].insert(0, None)
-        current_memory_size = int((xarrays_sized_cumulative_base + xarrays_sized_cumulative_mul * chunk_sizes_no_apply[dimname]))
         if current_memory_size != sum(chunks_memory_sizes):
             raise ValueError('inconsistency in de dimension selection size calculation')
-
-
     else:
         for ixarray,xarray in enumerate(xarrays):
             chunks_memory_sizes_dim[ixarray].insert(0,1)
@@ -215,10 +226,23 @@ chunks_number_no_apply = {}
 for dimname,shape in dims_no_apply_lengths.items():
     chunks_number_no_apply[dimname] = np.ceil(dims_no_apply_lengths[dimname]/chunk_sizes_no_apply[dimname])
 
+logging.info('memory input size of chunks: '+ str(current_memory_size) +'/'+str(maximum_memory_size_input) +' = '+str(current_memory_size/int(maximum_memory_size_input)*100)+'% of maximum')
+if current_memory_size > maximum_memory_size_input:
+    logging.critical('critical. I cannot fix chunks into memory. Please consider using (smaller) chunksize '
+                 'for apply output dimensions')
+
+
+#import pdb; pdb.set_trace()
+
 logging.info('Start looping over chunks and create selection from xarray inputs')
 
 chunks_no_apply = list(product(*tuple([list(range(int(a))) for a in list(chunks_number_no_apply.values())])))
-for index_no_apply in chunks_no_apply:
+for index_no_apply in tqdm.tqdm(chunks_no_apply):
+    logging.debug('dimension selection for '+\
+                  str(dims_no_apply.keys)+\
+                  ' with shape '+\
+                  str(dims_no_apply_lengths.values())+\
+                  ': '+str(index_no_apply))
 
     chunk_start = []
     chunk_end = []
@@ -229,10 +253,19 @@ for index_no_apply in chunks_no_apply:
     for dimname_apply in list(reversed(dims_apply_names)):
         if dimname_apply in number_of_chunks_apply_dims.keys():
             dim_apply_start = np.mod(idx_mod/dim_fac, number_of_chunks_apply_dims[dimname_apply] )
-            chunk_start.insert(0,dim_apply_start * dims_output[dimname_apply]['chunksize'])
-            chunk_end.insert(0,chunk_start[0]+  dims_output[dimname_apply]['chunksize'])
+            chunk_start.insert(0,int(dim_apply_start * dims_output[dimname_apply]['chunksize']))
+            chunk_end.insert(0,int(chunk_start[0]+  dims_output[dimname_apply]['chunksize']))
             idx_mod -= dim_apply_start*dim_fac
             dim_fac *= number_of_chunks_apply_dims[dimname_apply]
+
+            # chunk_start.insert(0,[])
+            # chunk_end.insert(0,[])
+            # for ichunk in range(int(chunks_number_no_apply[dimname])):
+            #     dim_apply_start = np.mod(idx_mod/dim_fac, number_of_chunks_apply_dims[dimname_apply] )
+            #     chunk_start[0].append(int(dim_apply_start * dims_output[dimname_apply]['chunksize']))
+            #     chunk_end[0].append(int(chunk_start[0]+  dims_output[dimname_apply]['chunksize']))
+            #     idx_mod -= dim_apply_start*dim_fac
+            #     dim_fac *= number_of_chunks_apply_dims[dimname_apply]
         else:
             chunk_start.insert(0,None)
             chunk_end.insert(0,None)
@@ -240,9 +273,8 @@ for index_no_apply in chunks_no_apply:
     for idim in reversed(range(len(index_no_apply))):
         dimname = list(dims_no_apply_lengths.keys())[idim]
         if dimname != '__chunk__':
-            chunk_start.insert(0,chunk_sizes_no_apply[dimname] * index_no_apply[idim])
-            chunk_end.insert(0,chunk_start[0] + chunk_sizes_no_apply[dimname])
-
+            chunk_start.insert(0,int(chunk_sizes_no_apply[dimname] * index_no_apply[idim]))
+            chunk_end.insert(0,int(chunk_start[0] + chunk_sizes_no_apply[dimname]))
         else:
             chunk_start.insert(0,None)
             chunk_end.insert(0,None)
@@ -250,8 +282,8 @@ for index_no_apply in chunks_no_apply:
     xarrays_selection_chunk = []
     for ixarray, xarray in enumerate(xarrays):
         xarrays_selection_chunk.append({})
-        for idim in range(len(dims_all)):
-            dimname = list(dims_no_apply_lengths.keys())[idim]
+        for idim,dimname in enumerate(dims_all):
+            #dimname = list(dims_no_apply_lengths.keys())[idim]
             if xarrays_shapes_in_chunks[ixarray][idim] is not None:
                 if dimname != '__chunk__':
                     if (dimname in dims_apply_names) and \
@@ -261,8 +293,8 @@ for index_no_apply in chunks_no_apply:
                         xarrays_selection_chunk[ixarray][dimname] = range(chunk_start[idim],chunk_end[idim])
                     elif dimname in dims_no_apply.keys():
                         xarrays_selection_chunk[ixarray][dimname] = range(chunk_start[idim],chunk_end[idim])
-                    else:
-                        xarrays_selection_chunk[ixarray][dimname] = None
+                    # else:
+                    #     xarrays_selection_chunk[ixarray][dimname] = None
         # for idim in range(len(index_no_apply)):
         #     dimname = list(dims_no_apply_lengths.keys())[idim]
         #     if xarrays_shapes_in_chunks[ixarray][idim] is not None:
@@ -275,7 +307,7 @@ for index_no_apply in chunks_no_apply:
         #                 xarrays_selection_chunk[ixarray][dimname] = range(chunk_start,chunk_end)
         #             else:
         #                 xarrays_selection_chunk[ixarray][dimname] = None
-    import pdb; pdb.set_trace()
+    logging.debug('xarray selection: '+str(xarrays_selection_chunk))
 
                         # number_of_chunks = []
 # for ixarray in range(len(xarrays)):
